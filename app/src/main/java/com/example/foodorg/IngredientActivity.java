2646d9c8db2498317767044abc3ae92a7c8b9bec
AMMAND3 <amman1@ualberta.ca>
2022-10-29 21:22:44 -0600

    Added functionality with Firestore - View, Add

--- app/src/main/java/com/example/foodorg/IngredientActivity.java
+++ app/src/main/java/com/example/foodorg/IngredientActivity.java
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,9 +22,12 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentChange;
@@ -39,96 +43,83 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-public class IngredientActivity extends AppCompatActivity implements IngredientAdapter.OnEditListener {
-    Button add;
-    Button returnButt;
-    ArrayList<Ingredient> FoodModelArrayList;
-    RecyclerView Food;
-    IngredientAdapter food;
-    String location;
+public class IngredientActivity extends AppCompatActivity implements IngredientAdapter.OnEditListner{
+
+    private Button returnHome;
+    private Button add;
+    private Button edit;
+
+
+    private RecyclerView recyclerView;
+    private FirebaseFirestore db;
+
+    private IngredientAdapter ingredientAdapter;
+    private List<IngredientModel> ingredientModelList;
+
     AlertDialog alertDialog;
-    FirebaseFirestore fStore;
-    String userID;
 
+    String userID;
     private FirebaseAuth mAuth;
+
+    //String user = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ingredient);
-        Food = findViewById(R.id.ingredientListRecyclerView);
 
-
-        FoodModelArrayList = new ArrayList<Ingredient>();
-        food = new IngredientAdapter(FoodModelArrayList,IngredientActivity.this,this);
-        Food.setAdapter(food);
-
-        mAuth = FirebaseAuth.getInstance();
-        fStore = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
         userID = mAuth.getCurrentUser().getUid();
 
-        add = findViewById(R.id.AddButtonIngredientStorage);
-
-        add.setOnClickListener(new View.OnClickListener() {
+        returnHome = findViewById(R.id.returnButtonIngredientStorage);
+        returnHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // the add button opens dialog to enter information
-                showCustomDialog();
-            }
-        });
-
-        returnButt = findViewById(R.id.returnButtonIngredientStorage);
-        returnButt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(IngredientActivity.this,HomePageActivity.class);
+                Intent i = new Intent(IngredientActivity.this, HomePageActivity.class);
                 startActivity(i);
             }
         });
 
-        CollectionReference collectionReference = fStore.collection("Users");
-
-
-        collectionReference.document(userID).collection("Ingredients").addSnapshotListener(new EventListener<QuerySnapshot>() {
-
+        add = findViewById(R.id.AddButtonIngredientStorage);
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
-                FoodModelArrayList.clear();
-                if (error != null){
-                    Log.e("Firestore error",error.getMessage());
-                    return;
-                }
-
-                for(QueryDocumentSnapshot doc: queryDocumentSnapshots)
-                {
-
-                    //String name = doc.getId();
-                    String name = (String) doc.getData().get("Title");
-                    String count = (String) doc.getData().get("Count");
-                    FoodModelArrayList.add(new Ingredient(name, count)); // Adding the cities and provinces from FireStore
-                }
-
-                food.notifyDataSetChanged();
-
+            public void onClick(View v) {
+                showCustomDialog();
+                Toast.makeText(IngredientActivity.this, "Dialog shown", Toast.LENGTH_SHORT).show();
             }
-
-
         });
+
+
+        recyclerView = findViewById(R.id.ingredientListRecyclerView);
+        //recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        db = FirebaseFirestore.getInstance();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        ingredientModelList = new ArrayList<>();
+        ingredientAdapter = new IngredientAdapter(this, ingredientModelList,this::onEditClick);
+
+        recyclerView.setAdapter(ingredientAdapter);
+
+//        CollectionReference collectionReference = db.collection("users")
+
+        showData();
 
 
     }
 
 
-    //dialog to add
-    private void showCustomDialog() {
-        //what to display on dialog
+    private void showCustomDialog(){
+
         final Dialog dialog = new Dialog(IngredientActivity.this);
+
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setCancelable(true);
         dialog.setContentView(R.layout.input_layout);
-
 
         //initializing edit text
         final EditText namek = dialog.findViewById(R.id.item_input);
@@ -145,89 +136,98 @@ public class IngredientActivity extends AppCompatActivity implements IngredientA
                 dialog.dismiss();
             }
         });
-        CollectionReference collectionReference = fStore.collection("Users");
-        //Location spinner
 
-        //submit button functionalities
         Button submitButton = dialog.findViewById(R.id.btnEdit);
+
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 mAuth = FirebaseAuth.getInstance();
-                fStore = FirebaseFirestore.getInstance();
-                mAuth = FirebaseAuth.getInstance();
+                db = FirebaseFirestore.getInstance();
                 userID = mAuth.getCurrentUser().getUid();
-
+                CollectionReference collectionReference = db.collection("users");
 
                 String name = namek.getText().toString();
                 String count = countk.getText().toString();
 
+                if(TextUtils.isEmpty(name) || TextUtils.isEmpty(count)){
+                    Toast.makeText(IngredientActivity.this, "Please enter all values", Toast.LENGTH_SHORT).show();
+                    return;
+                }else {
 
-                //check for empty fields error
-                if (namek.length() == 0) {
-                    namek.setError("Enter name");
-                }else if(countk.length()==0){
-                    countk.setError("Enter count");
-                } else {
-                    addFood(name,count);
-                    dialog.dismiss();
+                    System.out.println(ingredientModelList.size());
+                    IngredientModel ingredientModel = new IngredientModel(name, count);
+                    ingredientModelList.add(ingredientModel);
+                    System.out.println(ingredientModelList.size());
 
-                    HashMap<String,String> data = new HashMap<>();
-                    data.put("Title",name);
-                    data.put("Count",count);
-                    collectionReference.document(userID).collection("Ingredients")
-                            .add(data)
-                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    HashMap<String, Object> map = new HashMap<>();
+                    map.put("description", name);
+                    map.put("category", count);
+
+                    collectionReference.document(userID).collection("ingredient").document(name).set(map)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
-                                public void onSuccess(DocumentReference documentReference) {
-                                    Log.d(TAG, name + " has been added successfully!");
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+
+                                        dialog.dismiss();
+
+                                        Toast.makeText(IngredientActivity.this, "Ingredient added", Toast.LENGTH_SHORT).show();
+                                        ingredientAdapter.notifyDataSetChanged();
+
+                                    }
                                 }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
+                            }).addOnFailureListener(new OnFailureListener() {
                                 @Override
                                 public void onFailure(@NonNull Exception e) {
-                                    Log.d(TAG, name + " could not be added!");
+                                    Toast.makeText(IngredientActivity.this, "Failed", Toast.LENGTH_SHORT).show();
                                 }
                             });
 
-
                 }
+
             }
-
         });
+
         dialog.show();
-
-
-        //userID = mAuth.getCurrentUser().getUid();
-
-
 
     }
 
 
+    private void showData(){
 
+        CollectionReference collectionReference = db.collection("users");
+        collectionReference.document(userID).collection("ingredient").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
 
+                        ingredientModelList.clear();
 
-    public void addFood(String name, String count) {
+                        for (DocumentSnapshot snapshot : task.getResult()){
+                            IngredientModel ingredientModel = new IngredientModel(snapshot.getString("description"), snapshot.getString("category"));
+                            ingredientModelList.add(ingredientModel);
+                            //System.out.println(ingredientModelList.size());
 
-        Ingredient obj = new Ingredient(name,count);
-        obj.setDescription(name);
-        obj.setCount(count);
+                        }
 
-        FoodModelArrayList.add(obj);
-        Food = findViewById(R.id.ingredientListRecyclerView);
-        food = new IngredientAdapter(FoodModelArrayList, this, this::onEditClick);
-        LinearLayoutManager llm = new LinearLayoutManager(this);
-        llm.setOrientation(LinearLayoutManager.VERTICAL);
-        Food.setLayoutManager(llm);
-        Food.setAdapter(food);
+                        Toast.makeText(IngredientActivity.this, "Ingredients shown", Toast.LENGTH_SHORT).show();
+                        ingredientAdapter.notifyDataSetChanged();
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(IngredientActivity.this, "Oops, Something went wrong", Toast.LENGTH_SHORT).show();
+                    }
+                });
 
     }
 
     @Override
+    public void onEditClick(IngredientModel listData, int curPosition) {
 
-    public void onEditClick(Ingredient listData, int curPosition) {
         //Show dialog
         AlertDialog.Builder builderObj = new AlertDialog.Builder(this);
         View view = LayoutInflater.from(this).inflate(R.layout.input_layout, null);
@@ -236,48 +236,20 @@ public class IngredientActivity extends AppCompatActivity implements IngredientA
         closeAlert.setOnClickListener(v -> {
             alertDialog.cancel();
         });
+
         //Initializing
         Button btnEdit = view.findViewById(R.id.btnEdit);
         EditText na = view.findViewById(R.id.item_input);
         EditText co = view.findViewById(R.id.count_input);
         na.setText(listData.getDescription());
-        co.setText(listData.getCount());
+        co.setText(listData.getCategory());
 
 
         builderObj.setCancelable(false);
         builderObj.setView(view);
 
-        btnEdit.setOnClickListener(v -> {
-
-            String name = na.getText().toString();
-            String count = co.getText().toString();
-
-            String locate = location;
-            //Error checking for missing fields on edit
-            if (na.length() == 0) {
-                na.setError("Enter name");
-            }else if (co.length() == 0) {
-                co.setError("Enter name");
-
-            } else {
-                //editing the values and information
-                editContact(name,count, curPosition);
-            }
-        });
-
         alertDialog = builderObj.create();
         alertDialog.show();
-    }
-
-
-    //Editing to recycler view
-    public void editContact(String name, String count,int currentPosition){
-        Ingredient obj = new Ingredient(name,count);
-        obj.setDescription(name);
-        obj.setCount(count);
-        food.editDatalist(obj,currentPosition);
-        alertDialog.cancel();
-
 
     }
 
